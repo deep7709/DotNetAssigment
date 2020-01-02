9b9ad924c9bb48a60501c53618b4bd9d7088463c
nishantambaliya <nishantambaliya13@gmail.com>
2020-01-02 09:32:44 +0530

    initial commit

---
+++ Assignment1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern1();
+            Pattern2();
+            Pattern3();
+            Pattern4();
+            Console.Read();
+        }
+        static void Pattern1()
+        {
+            Console.WriteLine("Pattern1");
+            int i, j;
+            for (i = 0; i < 5; i++)
+            {
+                for (j = 0; j < 5; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        static void Pattern2()
+        {
+            Console.WriteLine("Pattern2");
+            int i, j;
+            for (i = 0; i < 5; i++)
+            {
+                for (j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        static void Pattern3()
+        {
+            Console.WriteLine("Pattern3");
+            int i, j;
+            for (i = 0; i < 5; i++)
+            {
+                for (j = i; j < 5; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        static void Pattern4()
+        {
+            Console.WriteLine("Pattern4");
+            int i, j, k;
+            for (i = 0; i < 5; i++)
+            {
+                for (k = 5-i; k > 1; k--)
+                {
+                    Console.Write(" ");
+                }
+                for (j = 0; j < 2*i+1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Assignment2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Text.RegularExpressions;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String userName, countryName;
+            Boolean flag;
+            Console.WriteLine("Please enter your name :");
+            userName = Console.ReadLine();
+            Console.WriteLine("Where are you from? :");
+            countryName = Console.ReadLine();
+
+            if (Regex.IsMatch(userName, @"^[a-zA-Z]+$") && Regex.IsMatch(countryName, @"^[a-zA-Z]+$"))
+                flag = true;
+            else
+                flag = false;
+            if (flag)
+                Console.WriteLine("Hello " + userName + " form " + countryName);
+            else
+                Console.WriteLine("Plese enter only characters");
+            Console.Read();
+        }
+    }
+}
